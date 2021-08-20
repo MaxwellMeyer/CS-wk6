370878949bf4bdf2ea9478018e022d573a2faa03
Maxwell Meyer <maxreadswell@gmail.com>
2021-08-20 14:20:07 -0700

    build get route for parks

--- ParksApi/Controllers/ParksController.cs
+++ ParksApi/Controllers/ParksController.cs
@@ -20,5 +20,34 @@ namespace ParkApi.Controllers
       _db = db;
     }
 
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get(string Name, string Description, string Facilities, string Region, string Stats)
+    {
+      var query = _db.Parks.AsQueryable();
+
+      if (Name != null)
+      {
+        query = query.Where(entry => entry.Name == Name);
+      }
+      if (Description != null)
+      {
+        query = query.Where(entry => entry.Description == Description);
+      }
+      if (Facilities != null)
+      {
+        query = query.Where(entry => entry.Facilities == Facilities);
+      }
+      if (Region != null)
+      {
+        query = query.Where(entry => entry.Region == Region);
+      }
+      if (Stats != null)
+      {
+        query = query.Where(entry => entry.Stats == Stats);
+      }
+      return await query.ToListAsync();
+    }
+
+
   }
 }
