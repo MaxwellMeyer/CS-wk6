8a1d3050085d69680bb69d551b9c6ab910899434
Maxwell Meyer <maxreadswell@gmail.com>
2021-08-20 13:43:23 -0700

    begin building controller

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkApi.Models;
+
+namespace ParkApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParkApiContext _db;
+
+    public ParksController(ParkApiContext db)
+    {
+      _db = db;
+    }
+
+  }
+}
